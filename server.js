5de994cce298e4a228e6e4ac4526b7b14f856fdd
TheWolverine796 <pandeymrityunjay796@gmail.com>
2023-03-20 23:19:18 +0530

    updated cors

--- server.js
+++ server.js
@@ -4,11 +4,30 @@ const dbConfig = require("./app/config/db.config");
 
 const app = express();
 
-var corsOptions = {
-  origin: "http://localhost:8081",
-};
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, X-Requested-With, Origin"
+  );
+  1;
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE, OPTIONS"
+  );
+  res.header("Access-Control-Expose-Headers", "X-Total-Count");
+  res.header("X-Total-Count", 500);
+  next();
+});
 
-app.use(cors(corsOptions));
+app.use(
+  cors({
+    credentials: true,
+    origin: true,
+  })
+);
+
+app.options("*", cors());
 
 // parse requests of content-type - application/json
 app.use(express.json());
